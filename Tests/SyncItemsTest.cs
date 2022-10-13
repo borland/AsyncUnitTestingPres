ad0db3c2c34dc5a631be385aa7ce25e8bef225c8
borland <orion.edwards@gmail.com>
2022-10-14 09:39:11 +1300

    first really basic part: Use TaskCompletionSource to stall the program and return control flow to the test

--- Tests/SyncItemsTest.cs
+++ Tests/SyncItemsTest.cs
@@ -20,41 +20,45 @@ public class SyncItemsTest
             ["item1.txt"] = Encoding.ASCII.GetBytes("Hello"),
             ["item2.txt"] = Encoding.ASCII.GetBytes("World"),
         };
-
-        httpClient.Enqueue(async (r) =>
-        {
-            r.Method.Should().Be(HttpMethod.Post);
-            r.RequestUri.ToString()!.Should().Be("/api/session");
-            var body = await r.Content.ReadAsStringAsync();
-            
-            var loginRequest = JsonSerializer.Deserialize<LoginRequest>(body);
-            loginRequest.Should().Be(new LoginRequest("Orion", "secret"));
-            return new HttpResponseMessage();
-        });
         
-        await Program.SyncItems(httpClient, localItems, cancellationToken);
+        var outerSyncTask = Program.SyncItems(httpClient, localItems, cancellationToken); // NOTE NO AWAIT
 
-        httpClient.RequestGenerators.Should().BeEmpty();
+        var r = httpClient.CurrentRequest?.Request;
+        r.Should().NotBeNull();
+        r.Method.Should().Be(HttpMethod.Post);
+        r.RequestUri.ToString()!.Should().Be("/api/session");
+        var body = await r.Content.ReadAsStringAsync();
+            
+        var loginRequest = JsonSerializer.Deserialize<LoginRequest>(body);
+        loginRequest.Should().Be(new LoginRequest("Orion", "secret"));
+        httpClient.CurrentRequest.Response.SetResult(new HttpResponseMessage());
+
+        await outerSyncTask;
+
+        // httpClient.RequestGenerators.Should().BeEmpty();
     }
 }
 
+public record MockRequestContext(HttpRequestMessage Request, TaskCompletionSource<HttpResponseMessage> Response); 
+
 public class MockHttpClient : IHttpClient
 {
-    public Queue<Func<HttpRequestMessage, Task<HttpResponseMessage>>> RequestGenerators {get;} = new();
+    private TaskCompletionSource<HttpResponseMessage> _nextResponder = new();
+    
+    public MockRequestContext? CurrentRequest { get; private set; }
 
-    public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        if (RequestGenerators.Count == 0)
+        TaskCompletionSource<HttpResponseMessage> responder;
+        lock (this)
         {
-            throw new InvalidOperationException($"Test failure; Received {request.Method} {request.RequestUri} but there was not a canned response for it");
+            responder = _nextResponder;
+
+            CurrentRequest = new(request, responder);
+
+            _nextResponder = new();
         }
-        
-        var gen = RequestGenerators.Dequeue();
-        var response = await gen(request);
 
-        return response;
+        return responder.Task;
     }
-
-    public void Enqueue(Func<HttpRequestMessage, Task<HttpResponseMessage>> generator)
-        => RequestGenerators.Enqueue(generator);
 }
