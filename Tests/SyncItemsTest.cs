ec804dd17eda162cd93993e184e02cb0cc3b61be
borland <orion.edwards@gmail.com>
2022-10-14 09:52:27 +1300

    This almost works! Thread-jumping is getting in the way

--- Tests/SyncItemsTest.cs
+++ Tests/SyncItemsTest.cs
@@ -23,19 +23,54 @@ public class SyncItemsTest
         
         var outerSyncTask = Program.SyncItems(httpClient, localItems, cancellationToken); // NOTE NO AWAIT
 
-        var r = httpClient.CurrentRequest?.Request;
-        r.Should().NotBeNull();
-        r.Method.Should().Be(HttpMethod.Post);
-        r.RequestUri.ToString()!.Should().Be("/api/session");
-        var body = await r.Content.ReadAsStringAsync();
-            
-        var loginRequest = JsonSerializer.Deserialize<LoginRequest>(body);
-        loginRequest.Should().Be(new LoginRequest("Orion", "secret"));
-        httpClient.CurrentRequest.Response.SetResult(new HttpResponseMessage());
+        (await httpClient.ExpectJsonRequest(HttpMethod.Post, "/api/session", new LoginRequest("Orion", "secret")))
+            .Response.SetResult(new HttpResponseMessage());
+        
+        (await httpClient.ExpectRequest(HttpMethod.Get, "/api/items"))
+            .Response.SetResult(new HttpResponseMessage{ Content = new StringContent(JsonSerializer.Serialize(new string[0]))});
+        
+        var ctx = await httpClient.ExpectRequest(HttpMethod.Post, "/api/files?name=file1.txt");
+        var s = await ctx.Request.Content.ReadAsStringAsync();
+        s.Should().Be("Hello");
+        ctx.Response.SetResult(new HttpResponseMessage());
+        
+        ctx = await httpClient.ExpectRequest(HttpMethod.Post, "/api/files?name=file2.txt");
+        s = await ctx.Request.Content.ReadAsStringAsync();
+        s.Should().Be("World");
+        ctx.Response.SetResult(new HttpResponseMessage());
 
         await outerSyncTask;
 
         // httpClient.RequestGenerators.Should().BeEmpty();
+    }
+}
+
+public static class MockHttpClientExtensions
+{
+    public static async Task<MockRequestContext> ExpectRequest(this MockHttpClient httpClient, HttpMethod method, string url)
+    {
+        var ctx = httpClient.CurrentRequestContext;
+        ctx.Should().NotBeNull();
+        
+        ctx.Request.Should().NotBeNull();
+        ctx.Request.Method.Should().Be(method);
+        ctx.Request.RequestUri.ToString().Should().Be(url);
+
+        return ctx;
+    }
+    
+    public static async Task<MockRequestContext> ExpectJsonRequest<TRequestBody>(this MockHttpClient httpClient, HttpMethod method, string url, TRequestBody? body = default)
+    {
+        var ctx = await ExpectRequest(httpClient, method, url);
+
+        if (body != null)
+        {
+            var requestBodyString = await ctx.Request.Content.ReadAsStringAsync();
+            var requestBodyObj = JsonSerializer.Deserialize<TRequestBody>(requestBodyString);
+            requestBodyObj.Should().BeEquivalentTo(body);
+        }
+
+        return ctx;
     }
 }
 
@@ -45,7 +80,7 @@ public class MockHttpClient : IHttpClient
 {
     private TaskCompletionSource<HttpResponseMessage> _nextResponder = new();
     
-    public MockRequestContext? CurrentRequest { get; private set; }
+    public MockRequestContext? CurrentRequestContext { get; private set; }
 
     public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
@@ -54,7 +89,7 @@ public class MockHttpClient : IHttpClient
         {
             responder = _nextResponder;
 
-            CurrentRequest = new(request, responder);
+            CurrentRequestContext = new(request, responder);
 
             _nextResponder = new();
         }
