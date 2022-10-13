7176c08ceafc46eefe692f4c307fc49a11ca9fc0
borland <orion.edwards@gmail.com>
2022-10-14 00:05:24 +1300

    This is what you have to do to actually assert everything properly

--- Tests/SyncItemsTest.cs
+++ Tests/SyncItemsTest.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text;
 using System.Text.Json;
 using AsyncUnitTestingPres;
 using FluentAssertions;
@@ -8,19 +9,42 @@ namespace Tests;
 public class SyncItemsTest
 {
     [Fact]
-    public async Task SyncsNoItems()
+    public async Task SyncsTwoItems()
     {
         var httpClient = new MockHttpClient();
         var cancellationToken = CancellationToken.None;
 
         httpClient.RespondWith(HttpMethod.Post, "/api/session", HttpStatusCode.Created);
-        httpClient.RespondWithJson(HttpMethod.Get, "/api/items", HttpStatusCode.OK, new[]{"item1.txt", "item2.txt"});
+        httpClient.RespondWithJson(HttpMethod.Get, "/api/items", HttpStatusCode.OK, new string[0]);
         httpClient.RespondWith(HttpMethod.Post, "/api/items?name=item1.txt", HttpStatusCode.OK);
         httpClient.RespondWith(HttpMethod.Post, "/api/items?name=item2.txt", HttpStatusCode.OK);
-        
-        await Program.SyncItems(httpClient, new Dictionary<string, byte[]>(), cancellationToken);
 
+        var localItems = new Dictionary<string, byte[]>
+        {
+            ["item1.txt"] = Encoding.ASCII.GetBytes("Hello"),
+            ["item2.txt"] = Encoding.ASCII.GetBytes("World"),
+        };
+        
+        await Program.SyncItems(httpClient, localItems, cancellationToken);
+        
         httpClient.ReceivedRequests.Count.Should().Be(4);
+
+        AssertRequest(httpClient.ReceivedRequests[0], HttpMethod.Post, "/api/session");
+        AssertRequest(httpClient.ReceivedRequests[1], HttpMethod.Get, "/api/items");
+        AssertRequest(httpClient.ReceivedRequests[2], HttpMethod.Post, "/api/items?name=item1.txt");
+        
+        var r2 = await httpClient.ReceivedRequests[2].Request.Content.ReadAsStringAsync();
+        r2.Should().Be("Hello");
+        
+        AssertRequest(httpClient.ReceivedRequests[3], HttpMethod.Post, "/api/items?name=item2.txt");
+        var r3 = await httpClient.ReceivedRequests[3].Request.Content.ReadAsStringAsync();
+        r3.Should().Be("World");
+    }
+
+    static void AssertRequest(ReceivedRequest req, HttpMethod expectedMethod, string expectedUrl)
+    {
+        req.Method.Should().Be(expectedMethod);
+        req.Url.Should().Be(expectedUrl);
     }
 }
 
