f99f0f27b8614dc0ad9a55baa0e9dba74d92085b
borland <orion.edwards@gmail.com>
2022-10-13 23:58:00 +1300

    assert on received requests

--- Tests/SyncItemsTest.cs
+++ Tests/SyncItemsTest.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Text.Json;
 using AsyncUnitTestingPres;
+using FluentAssertions;
 
 namespace Tests;
 
@@ -18,18 +19,26 @@ public class SyncItemsTest
         httpClient.RespondWith(HttpMethod.Post, "/api/items?name=item2.txt", HttpStatusCode.OK);
         
         await Program.SyncItems(httpClient, new Dictionary<string, byte[]>(), cancellationToken);
+
+        httpClient.ReceivedRequests.Count.Should().Be(4);
     }
 }
+
+public record struct ReceivedRequest(HttpMethod Method, string Url, HttpRequestMessage Request);
 
 public class MockHttpClient : IHttpClient
 {
     record struct RequestKey(HttpMethod Method, string Url);
 
     private Dictionary<RequestKey, HttpResponseMessage> CannedResponses = new();
+    
+    public List<ReceivedRequest> ReceivedRequests {get;} = new();
 
     public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         var requestKey = new RequestKey(request.Method, request.RequestUri!.ToString());
+        ReceivedRequests.Add(new(requestKey.Method, requestKey.Url, request));
+        
         if (CannedResponses.TryGetValue(requestKey, out var responseMessage))
             return Task.FromResult(responseMessage);
 
