c885f9d7c70566dd3148bd9a769afb871d5ea56f
borland <orion.edwards@gmail.com>
2022-10-14 14:06:47 +1300

    Make it work

--- Tests/SyncItemsTest.cs
+++ Tests/SyncItemsTest.cs
@@ -29,19 +29,17 @@ public class SyncItemsTest
         (await httpClient.ExpectRequest(HttpMethod.Get, "/api/items"))
             .Response.SetResult(new HttpResponseMessage{ Content = new StringContent(JsonSerializer.Serialize(new string[0]))});
         
-        var ctx = await httpClient.ExpectRequest(HttpMethod.Post, "/api/files?name=file1.txt");
-        var s = await ctx.Request.Content.ReadAsStringAsync();
+        var ctx = await httpClient.ExpectRequest(HttpMethod.Post, "/api/items?name=item1.txt");
+        var s = await ctx.Request.Content!.ReadAsStringAsync();
         s.Should().Be("Hello");
         ctx.Response.SetResult(new HttpResponseMessage());
         
-        ctx = await httpClient.ExpectRequest(HttpMethod.Post, "/api/files?name=file2.txt");
-        s = await ctx.Request.Content.ReadAsStringAsync();
+        ctx = await httpClient.ExpectRequest(HttpMethod.Post, "/api/items?name=item2.txt");
+        s = await ctx.Request.Content!.ReadAsStringAsync();
         s.Should().Be("World");
         ctx.Response.SetResult(new HttpResponseMessage());
 
         await outerSyncTask;
-
-        // httpClient.RequestGenerators.Should().BeEmpty();
     }
 }
 
@@ -49,7 +47,7 @@ public static class MockHttpClientExtensions
 {
     public static async Task<MockRequestContext> ExpectRequest(this MockHttpClient httpClient, HttpMethod method, string url)
     {
-        var ctx = httpClient.CurrentRequestContext;
+        var ctx = await httpClient.NextRequest();
         ctx.Should().NotBeNull();
         
         ctx.Request.Should().NotBeNull();
@@ -78,22 +76,75 @@ public record MockRequestContext(HttpRequestMessage Request, TaskCompletionSourc
 
 public class MockHttpClient : IHttpClient
 {
-    private TaskCompletionSource<HttpResponseMessage> _nextResponder = new();
-    
-    public MockRequestContext? CurrentRequestContext { get; private set; }
+    private readonly Queue<MockRequestContext> _requests = new();
 
-    public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    // Approximation of an AutoResetEvent using Tasks
+    private TaskCompletionSource<bool> _requestEnqueued = new();
+    
+    public async Task<MockRequestContext> NextRequest()
     {
-        TaskCompletionSource<HttpResponseMessage> responder;
+        MockRequestContext? ctx;
+        do
+        {
+            lock (this)
+            {
+                // if the SUT called SendAsync first, there'll be a request context sitting waiting for us that we can just use
+                _requests.TryDequeue(out ctx);
+            }
+
+            if (ctx == null)
+            {
+                // else we have to wait for the SUT to call SendAsync
+                await _requestEnqueued.Task;
+                lock (this)
+                {
+                    _requestEnqueued = new();
+                }
+            }
+
+        } while (ctx == null);
+        
+        return ctx;
+    }
+
+    public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        // the SUT has made a request. Trigger the stalled unit test which should be waiting on NextRequest
+        var responder = new TaskCompletionSource<HttpResponseMessage>();
+        
         lock (this)
         {
-            responder = _nextResponder;
-
-            CurrentRequestContext = new(request, responder);
-
-            _nextResponder = new();
+            _requests.Enqueue(new MockRequestContext(request, responder));
         }
+        _requestEnqueued.TrySetResult(true);
+        
+        // the unit test will now proceed. Eventually it should call SetResult on responder, in which case we can unblock the SUT
+        var response = await responder.Task;
 
-        return responder.Task;
+        return response;
     }
 }
+
+/* Flows:
+1: Unit test goes first:
+ - Test calls NextRequest
+ - Test tries to dequeue a CTX, there isn't one
+ - Test stalls on _requestEnqueued
+ - SUT eventually calls SendAsync
+ - SUT enqueues a new CTX and sets _requestEnqueued
+ - SUT stalls on ctx.responder
+ - Test resumes, does stuff (assertions etc)
+ - Test sets ctx.responder and proceeds freely to the next part
+ - SUT resumes, handles the response and proceeds freely to the next part
+ 
+2: SUT goes first:
+ - SUT calls SendAsync
+ - SUT enqueues a new CTX and sets _requestEnqueued (nobody listening so who cares)
+ - SUT stalls on ctx.responder
+ - Test calls NextRequest
+ - Test tries to dequeue a CTX, gets one
+ - Test does stuff (assertions etc)
+ - Test sets ctx.responder and proceeds freely to the next part
+ - SUT resumes, handles the response and proceeds freely to the next part
+*/
+
